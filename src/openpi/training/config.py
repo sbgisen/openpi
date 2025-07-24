b8d9edd64affbc3a422004d2594787c11f75891c
Tatsuro Sakaguchi <tatsuro.sakaguchi@g.softbank.co.jp>
2025-07-24 13:21:15 +0900

    Update key

--- src/openpi/training/config.py
+++ src/openpi/training/config.py
@@ -478,7 +478,7 @@ class LeRobotSoarDataConfig(DataConfigFactory):
                     {
                         'observation/head_image': 'head_image',
                         'observation/hand_image': 'hand_image',
-                        'observation/state': 'state',
+                        'observation/state': 'observation.state',
                         'actions': 'actions',
                         'prompt': 'prompt',
                     }
